d3fd3a1c815f84e334f1058bab61f4635e40a045
Gaming-Priest <tekpriest.org@gmail.com>
2021-01-06 01:14:35 +0100

    build

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.less";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../fontawesome@5.15.1/css/all.css'
+// import 'bootstrap/dist/css/bootstrap.css';
+// import '../fontawesome@5.15.1/css/all.css'
 
 
 var mountNode = document.getElementById("app");
